15905af4a23b78d23e11f0cb60424f322941064b
Hamburger-coder <joesmast@gmail.com>
2025-01-21 19:56:14 -0500

    Add the ability to open and close dialog, gather information from the form, and log it.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,73 @@
 import "./styles.css";
 import { greeting } from "./greeting.js";
 
+// add todo button
+const showBtn = document.getElementById("show-dialog");
+// The dialog for adding todos
+const dialog = document.getElementById("dialog");
+// Close button for the dialog
+const jsCloseBtn = document.getElementById("js-close");
+// Submit button
+const submitBtn = document.getElementsByName("submit");
+// The form for adding todos
+const todoForm = document.getElementById("todoForm");
+// List of todos
+const todos = [];
+
+
+// Event listener for showing dialog
+showBtn.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+// Event listener for closing the dialog
+jsCloseBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    dialog.close();
+});
+
+
+// Event listener for when the user clicks outside the dialog
+dialog.addEventListener("click", (event) => {
+    if (event.target === dialog) {
+        dialog.close();
+    }
+});
+
+
+// Event listener for the Delete button 
+
+
+// Event listener for the form submission button
+todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(todoForm);
+    const todoData = {};
+    formData.forEach((value, key) => {
+        todoData[key] = value;
+    });
+    console.log(todoData.title, todoData.description, todoData.dueDate, todoData.priority, todoData.notes);
+    addTodo(todoData.title, todoData.description, todoData.dueDate, todoData.priority, todoData.notes)
+    dialog.close();
+
+})
+
+
 console.log(greeting);
+
+function addTodo (title, description, dueDate, priority, note) {
+    const newTodo = new Todo(title, description, dueDate, priority, note)
+    todos.push(newTodo);
+}
+
+function Todo (title, description, dueDate, priority, note) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.note = note;
+}
+
+console.log(todos);
 
